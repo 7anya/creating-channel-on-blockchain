cec3fa9d0d121ce4b6e58ea7aee94a2554fbc7c4
Tanya Prasad <55975635+maruyari@users.noreply.github.com>
2020-06-30 16:57:15 +0530

    done

--- ps_project_2/main.go
+++ ps_project_2/main.go
@@ -11,6 +11,7 @@ import (
 func CreateChannel(c *gin.Context) {
 	name := c.Query("name")
 	output, err:= exec.Command("./createchannel.sh", name).Output()
+	c.Writer.Header().Set("Access-Control-Allow-Origin","*")
 	if err != nil {
 		fmt.Println(err)
 		c.JSON(http.StatusOK, gin.H{"err": err})
@@ -26,7 +27,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/",CreateChannel)
-
+	r.Static("/form",".")
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
